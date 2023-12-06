782d093f23047cff82ea2ea2c93ee98d84a4d677
d1erro <d1erro7@gmail.com>
2023-12-06 22:31:29 +0300

    Fix theme provider

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,7 +1,7 @@
-import { LOCAL_STORAGE_THEME_KEY, type Theme, ThemeContext } from '../lib/ThemeContext'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext'
 import { type ReactNode, useMemo, useState } from 'react'
 
-const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme ?? Theme.LIGHT
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme)
